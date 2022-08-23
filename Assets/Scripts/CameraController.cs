048c992205948614f824ac2f3d669a5b7f759170
ensarakkuzey <ensarakkuzey@gmail.com>
2022-08-23 16:56:49 +0300

    Create Camera Controller script and new folders.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(_player.position.x, _player.position.y, transform.position.z);
+    }
+}
